bfa5f303ffd5a5a036c30a82ec62eb9ba5ec5d9f
unukuu <ll040299@gmail.com>
2024-08-28 12:57:13 +0800

    router

---
+++ backend/routes/user-route.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+
+const router = router();
+
+router.get();
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,44 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { logger } = require("./middlewares/logger");
+dotenv.config();
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(logger());
+
+app.get("/user", (req, res) => {
+  console.log("get ");
+  res.json({ message: "success" });
+});
+app.post("/user", (req, res) => {
+  console.log("post");
+});
+app.put("/user", (req, res) => {
+  console.log("changed users");
+});
+app.delete("/user", (req, res) => {
+  console.log("deleted");
+});
+
+app.get("/category", (req, res) => {
+  console.log("get ");
+});
+app.post("/category", (req, res) => {
+  console.log("post");
+});
+app.put("/category", (req, res) => {
+  console.log("changed category");
+});
+app.delete("/category", (req, res) => {
+  console.log("deleted");
+});
+
 app.listen(PORT, () => {
-  console.log("ajiljiinaa");
+  console.log(`server is running at ~localhost:${PORT}`);
 });
