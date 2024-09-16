473dc36c060f5ae51794e3765d7e71a951985bb0
unukuu <ll040299@gmail.com>
2024-09-16 11:51:44 +0800

    commit 1dh

--- frontend/src/app/(main)/dashboard/records.jsx
+++ frontend/src/app/(main)/dashboard/records.jsx
@@ -1,7 +1,7 @@
 import Recordcard from "./recordlist";
 const RecordList = () => {
   return (
-    <div className="container m-auto bg-white ">
+    <div className="container m-auto bg-white h-[450px] overflow-scroll">
       <div className="border-b-2 px-6 py-4">
         <h1 className="font-bold">Last Records</h1>
       </div>
